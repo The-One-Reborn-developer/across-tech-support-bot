b720efd8e1597d972373ab14d5c746e681600d21
The One Reborn <mortonmarrion@gmail.com>
2024-10-12 14:02:25 +0300

    fix: typo in import

--- app/database/models/async_session.py
+++ app/database/models/async_session.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import async_sessionmaker
 
-from app.database.models import async_engine
+from app.database.models.async_engine import async_engine
 
 
 async_session = async_sessionmaker(async_engine, expire_on_commit=False)
